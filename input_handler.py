25ae79ad3dae1f831328c053333108cf4359c104
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-12-05 15:11:44 -0300

    movimentação modificada

--- input_handler.py
+++ input_handler.py
@@ -1,10 +1,27 @@
 from pygame.locals import *
 import pygame as pg
 
-def check_event(player):
-    for event in pg.event.get():
+class InputHandler:
+    def __init__(self, player) -> None:
+        self._player = player
+
+    def update(self):
+        self.check_event()
+
+    def handle_event(self, event):
         if event.type == KEYDOWN:
-            if event.key == K_UP: player.jump()
+            if event.key == K_UP: self.player.jump()
+            if event.key == K_LEFT: self.player.move(-1)
+            if event.key == K_RIGHT: self.player.move(1)
+        
+        if event.type == KEYUP:
+            if event.key == K_LEFT: self.player.move(1)
+            if event.key == K_RIGHT: self.player.move(-1)
+
         if event.type == QUIT: 
             pg.quit()
             exit()
+
+    def check_event(self):
+        for event in pg.event.get():
+            self.handle_event(event)
--- player.py
+++ player.py
@@ -6,16 +6,18 @@ class Player(Entity):
         super().__init__(position)
         
 
-    def movement(self, direction):
-        self.direction.x = self.x_vel*direction
-        self.rect.x += self.direction.x
-    
+    def change_direction(self, direction):
+        self.direction.x += self.x_vel*direction
+        
+
     def jump(self):
         if self.on_ground:
             self.direction.y = -10
     
     def update(self):
         super().update()
+        self.rect.x += self.direction.x
+
         if self.rect.bottomleft[1] < 200:
             self.on_ground = False
             self.gravity = 0.8
