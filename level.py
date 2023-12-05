a3b87248b9883ce9654fb9504779a2ab327d4153
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-12-05 15:40:57 -0300

    test level modified

--- level.py
+++ level.py
@@ -2,38 +2,26 @@ import pygame as pg
 from platforms import Platform
 from player import Player
 from enemies import Enemies
-from input_handler import check_event
 
 class Level:
-    def __init__(self, player, enemie) -> None:
+    def __init__(self) -> None:
 
         self.platform_list = pg.sprite.Group()
-        self.player_list = pg.sprite.Group()
         self.enemie_list = pg.sprite.Group()
 
-        self.player = player
-        self.player_list.add(self.player)
-
-        self.enemie = enemie
-        self.enemie_list.add(self.enemie)
-
     def update(self):
-        check_event(self.player)
         self.platform_list.update()
-        self.player_list.update()
         self.enemie_list.update()
         
 
     def draw(self, screen: pg.Surface):
         screen.fill("white")
-
-        self.player_list.draw(screen)
         self.platform_list.draw(screen)
         self.enemie_list.draw(screen)
 
 class TestLevel(Level):
-    def __init__(self, player, enemie) -> None:
-        super().__init__(player, enemie)
+    def __init__(self) -> None:
+        super().__init__()
         
         for block in range(5):
             self.platform_list.add(Platform(70, 50, (100*block, 200)))
