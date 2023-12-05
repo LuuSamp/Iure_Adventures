b4b26d64ec5a93050e2de94abcaac47d514d828c
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-12-05 17:52:19 -0300

    collide_with_enemy method done

--- player.py
+++ player.py
@@ -5,7 +5,6 @@ class Player(Entity):
     def __init__(self, position):
         super().__init__(position)
         
-
     def move(self, direction):
         self.direction.x += self.x_vel*direction
 
@@ -13,6 +12,16 @@ class Player(Entity):
         if self.on_ground:
             self.direction.y = -10
     
+    def collide_with_enemy(self, enemy_group: pg.sprite.Group):
+        for enemy in pg.sprite.spritecollide(self, enemy_group, False):
+            if pg.sprite.collide_rect(self, enemy):
+                if (enemy.rect.collidepoint(self.rect.bottomright) or 
+                enemy.rect.collidepoint(self.rect.midbottom) or
+                enemy.rect.collidepoint(self.rect.bottomleft)):
+                    enemy.kill()
+                else:
+                    self.kill()
+
     def update(self):
         super().update()
         self.rect.x += self.direction.x
