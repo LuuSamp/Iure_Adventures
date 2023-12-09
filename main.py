2e618bb12144ffb34692e9412eeb1d19b7d0f55c
Luciano Sampaio <luciano.sampaio2004@gmail.com>
2023-12-09 20:43:52 -0300

    Updated main

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-import pygame
-import sys
-from level import Level
-from player import Player
-from input_handler import InputHandler
-from const import *
+from game import Game
 
-screen_width = SCREEN_WIDTH
-screen_height = SCREEN_HEIGHT
-
-# Pygame setup
-pygame.init()
-screen = pygame.display.set_mode((screen_width, screen_height))
-print(type(screen))
-clock = pygame.time.Clock()
-player = Player((200, 0))
-level = Level(screen, player)
-input_handler = InputHandler(player)
-while True:
-    input_handler.update()
-    
-    screen.blit(pygame.transform.scale(pygame.image.load("media/background/background_frame.png"), (SCREEN_WIDTH, SCREEN_HEIGHT)), (0,0))
-    level.run()
-
-    pygame.display.update()
-    clock.tick(FPS)
+game = Game()
+game.run()
