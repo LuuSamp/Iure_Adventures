aedb0a6e4e49a16378d7a05b947741e54d5c9b7f
Matheus <matheuscarvalho210404@outlook.com>
2023-12-09 14:39:50 -0300

    sincronizando a animação da princesa e da fumaça

--- princess.py
+++ princess.py
@@ -32,7 +32,12 @@ class PrincessPinho(pg.sprite.Sprite):
                         pg.image.load(path.join(self.princess_dir, "livre/pinho_solto2.png")),
                         pg.image.load(path.join(self.princess_dir, "livre/pinho_solto3.png")),
                         pg.image.load(path.join(self.princess_dir, "livre/pinho_solto4.png")),
-                        pg.image.load(path.join(self.princess_dir, "livre/pinho_solto5.png"))]]
+                        pg.image.load(path.join(self.princess_dir, "livre/pinho_solto5.png"))],
+                        
+                        [pg.image.load(path.join(self.princess_dir, "preso/pinho_preso_7.png")),
+                         pg.image.load(path.join(self.princess_dir, "preso/pinho_preso_8.png")),
+                         pg.image.load(path.join(self.princess_dir, "livre/pinho_solto0.png"))]
+                        ]
         
         self.animation_counter = 0
         self.animation_delay = 120
@@ -40,30 +45,52 @@ class PrincessPinho(pg.sprite.Sprite):
 
         self.image = pg.Surface((100, 100))
         self.rect = self.image.get_rect(center=position)
+        self.on_animation = True
 
     def change_list(self) -> None:
         """
         método para mudar a lista de animações
         """
-        self.animation_list = 1
+        self.animation_list = 2
+        self.on_animation = False
+        self.animation_counter = 0
         
     def animation(self) -> None:
         """
         método que controla as animações levando em consideração um delay
         """
-        if self.animation_counter >= self.animation_delay:
+        if self.on_animation:
+            if self.animation_counter >= self.animation_delay:
+                self.animation_counter = 0
+
+            frame = self.frames[self.animation_list][self.animation_counter // 20]
+            self.image = pg.transform.scale(frame, (100, 100))
+
+            self.animation_counter += 1
+
+    def animation_jail(self):
+        """
+        método para modelar a animação da cela caindo
+        """
+        if not self.on_animation:
+            frame = self.frames[self.animation_list][self.animation_counter // 20]
+            self.image = pg.transform.scale(frame, (100, 100))
+
+            self.animation_counter += 1
+
+        if self.animation_counter >= 60:
+            self.on_animation = True
+            self.on_animation = 0
             self.animation_counter = 0
-
-        frame = self.frames[self.animation_list][self.animation_counter // 20]
-        self.image = pg.transform.scale(frame, (100, 100))
-
-        self.animation_counter += 1
+            self.animation_list = 1
+        
 
     def update(self) -> None:
         """
         método de atualizações
         """
         self.animation()
+        self.animation_jail()
 
 class Smoke(pg.sprite.Sprite):
     """
@@ -93,20 +120,20 @@ class Smoke(pg.sprite.Sprite):
                        pg.image.load(path.join(self.princess_dir, "smoke/smoke_0.png")),
                        pg.image.load(path.join(self.princess_dir, "smoke/smoke_0.png"))]
         
-        self.smoke_counter = 40
+        self.smoke_counter = 60
 
     def animation(self) -> None:
         """
         método para modelar a animação da fumaça
         """
 
-        if self.smoke_counter < 8:
+        if self.smoke_counter < 15:
             frame = 0
-        elif self.smoke_counter < 16:
+        elif self.smoke_counter < 30:
             frame = 1
-        elif self.smoke_counter < 24:
+        elif self.smoke_counter < 45:
             frame = 2
-        elif self.smoke_counter < 32:
+        elif self.smoke_counter < 60:
             frame = 3
         else:
             self.kill()
